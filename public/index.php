bdb2aed061287296c71d96860694a587b777a57f
Noah2211 <noah.decroix3@gmail.com>
2022-02-01 13:55:24 +0100

    remove strtolower for server

--- public/index.php
+++ public/index.php
@@ -3,7 +3,7 @@ session_start();
 $page = $_GET['p'] ?? 'home';
 $page = strip_tags($page);
 
-checkPageExist(sprintf(__DIR__ . '/../%s.php', strtolower($page)));
+checkPageExist(sprintf(__DIR__ . '/../%s.php', $page));
 
 function checkPageExist(string$pageName) {
     require file_exists($pageName) ? $pageName : __DIR__ . '/../404.php';
